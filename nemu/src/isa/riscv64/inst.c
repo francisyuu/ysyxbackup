eca80e631ccda1f9bfacb7eecf98483cb4500c11
ysyx_22050133-Yu Hang <1339236493@qq.com>
2022-10-18 22:40:36 +0800

    dummy test passed,inst add:addi jal jalr

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -23,15 +23,16 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_R, TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J, 
   TYPE_N, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1)<<20)|BITS(i,19,12)<<12|BITS(i,20,20)<<11|BITS(i,30,21)<<1; } while(0)
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -40,10 +41,14 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
   int rs2 = BITS(i, 24, 20);
   *dest = rd;
   switch (type) {
-    case TYPE_I: src1R();          immI(); break;
-    case TYPE_U:                   immU(); break;
-    case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_R: src1R(); src2R();         break;
+    case TYPE_I: src1R();          immI(); break;//ori
+    case TYPE_S: src1R(); src2R(); immS(); break;//ori
+    case TYPE_B:                           break;
+    case TYPE_U:                   immU(); break;//ori
+    case TYPE_J:                   immJ(); break; 
   }
+  printf("rd=%d,rs1=%d,rs2=%d,imm=%ld\n",rd,rs1,rs2,(int64_t)*imm);
 }
 
 static int decode_exec(Decode *s) {
@@ -58,12 +63,15 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = s->pc + imm);
-  INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + imm, 8));
-  INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + imm, 8, src2));
+  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = s->pc + imm);//ori
+  INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + imm, 8));//ori
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + imm);
+  INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + imm, 8, src2));//ori
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest)=s->pc+4,s->dnpc=s->pc+imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(dest)=s->pc+4,s->dnpc=(src1+imm)&(~1));
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));//ori
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
